01e0158922e03d146f4365a5a67ff0ba4a9ed0de
Julio Benavente <julio.benavente.02@gmail.com>
2024-01-31 14:53:32 -0700

    forwarding ref to typography

--- src/components/Typography/index.tsx
+++ src/components/Typography/index.tsx
@@ -1,37 +1,40 @@
-import React from "react";
+import React, { forwardRef } from "react";
 import MuiTypography, { TypographyProps } from "@mui/material/Typography";
 import cn from "@/helpers/cn";
 
-const Typography = ({
-  className,
-  classes,
-  variant = "body1",
-  ...props
-}: TypographyProps) => {
-  return (
-    <MuiTypography
-      variant={variant}
-      classes={{
-        root: cn("font-primary"),
-        h1: cn(
-          "text-4xl lg:text-5xl font-cabinet text-text-headline font-medium"
-        ),
-        h2: cn("text-3xl lg:text-4xl text-text-headline font-medium"),
-        h3: cn("text-2xl lg:text-2xl text-text-headline font-medium"),
-        h4: cn("text-xl lg:text-xl font-bold text-text-headline"),
-        h5: cn("text-lg lg:text-lg font-bold text-text-headline"),
-        body1: cn("text-lg text-text"),
-        body2: cn("text-base text-text"),
-        subtitle1: cn("text-lg text-text"),
-        caption: cn("text-sm leading-snug"),
-        ...classes,
-      }}
-      className={cn(className)}
-      {...props}
-    >
-      {props.children}
-    </MuiTypography>
-  );
-};
+const Typography = forwardRef(
+  (
+    { className, classes, variant = "body1", ...props }: TypographyProps,
+    ref: any
+  ) => {
+    return (
+      <MuiTypography
+        ref={ref}
+        variant={variant}
+        classes={{
+          root: cn("font-primary"),
+          h1: cn(
+            "text-4xl lg:text-5xl font-cabinet text-text-headline font-medium"
+          ),
+          h2: cn("text-3xl lg:text-4xl text-text-headline font-medium"),
+          h3: cn("text-2xl lg:text-2xl text-text-headline"),
+          h4: cn("text-xl lg:text-xl font-bold text-text-headline"),
+          h5: cn("text-lg lg:text-lg font-bold text-text-headline"),
+          body1: cn("text-lg text-text"),
+          body2: cn("text-base text-text"),
+          subtitle1: cn("text-lg text-text"),
+          caption: cn("text-sm leading-snug"),
+          ...classes,
+        }}
+        className={cn(className)}
+        {...props}
+      >
+        {props.children}
+      </MuiTypography>
+    );
+  }
+);
+
+Typography.displayName = "Typography";
 
 export default Typography;
