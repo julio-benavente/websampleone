24d33401c3ebf87d5b4b2858cd0af12912ac0c93
Julio Benavente <julio.benavente.02@gmail.com>
2024-02-01 12:14:33 -0700

    data moved

---
+++ src/app/home/FAQSection/accordionItems.tsx
@@ -0,0 +1,128 @@
+import { Link } from "@/components";
+
+const accordionItems = [
+  {
+    id: "1",
+    question: "Who can become an The Good Doctor patient?",
+    answer: (
+      <>
+        Our doctors specialize caring for anyone with Medicare, including
+        Original Medicare Part B, select Medicare Advantage plans, Medicare
+        Supplement or{" "}
+        <Link href="/" className="link">
+          Medigap plans and Medicare-Medicaid
+        </Link>{" "}
+        Plans.
+      </>
+    ),
+  },
+  {
+    id: "2",
+    question: "Who can become an The Good Doctor patient?",
+    answer: (
+      <>
+        Our doctors specialize caring for anyone with Medicare, including
+        Original Medicare Part B, select Medicare Advantage plans, Medicare
+        Supplement or{" "}
+        <Link href="/" className="link">
+          Medigap plans and Medicare-Medicaid
+        </Link>{" "}
+        Plans.
+      </>
+    ),
+  },
+  {
+    id: "3",
+    question: "Who can become an The Good Doctor patient?",
+    answer: (
+      <>
+        Our doctors specialize caring for anyone with Medicare, including
+        Original Medicare Part B, select Medicare Advantage plans, Medicare
+        Supplement or{" "}
+        <Link href="/" className="link">
+          Medigap plans and Medicare-Medicaid
+        </Link>{" "}
+        Plans.
+      </>
+    ),
+  },
+  {
+    id: "4",
+    question: "Who can become an The Good Doctor patient?",
+    answer: (
+      <>
+        Our doctors specialize caring for anyone with Medicare, including
+        Original Medicare Part B, select Medicare Advantage plans, Medicare
+        Supplement or{" "}
+        <Link href="/" className="link">
+          Medigap plans and Medicare-Medicaid
+        </Link>{" "}
+        Plans.
+      </>
+    ),
+  },
+
+  {
+    id: "5",
+    question: "Who can become an The Good Doctor patient?",
+    answer: (
+      <>
+        Our doctors specialize caring for anyone with Medicare, including
+        Original Medicare Part B, select Medicare Advantage plans, Medicare
+        Supplement or{" "}
+        <Link href="/" className="link">
+          Medigap plans and Medicare-Medicaid
+        </Link>{" "}
+        Plans.
+      </>
+    ),
+  },
+  {
+    id: "6",
+    question: "Who can become an The Good Doctor patient?",
+    answer: (
+      <>
+        Our doctors specialize caring for anyone with Medicare, including
+        Original Medicare Part B, select Medicare Advantage plans, Medicare
+        Supplement or{" "}
+        <Link href="/" className="link">
+          Medigap plans and Medicare-Medicaid
+        </Link>{" "}
+        Plans.
+      </>
+    ),
+  },
+
+  {
+    id: "7",
+    question: "Who can become an The Good Doctor patient?",
+    answer: (
+      <>
+        Our doctors specialize caring for anyone with Medicare, including
+        Original Medicare Part B, select Medicare Advantage plans, Medicare
+        Supplement or{" "}
+        <Link href="/" className="link">
+          Medigap plans and Medicare-Medicaid
+        </Link>{" "}
+        Plans.
+      </>
+    ),
+  },
+  {
+    id: "8",
+    question: "Who can become an The Good Doctor patient?",
+    answer: (
+      <>
+        Our doctors specialize caring for anyone with Medicare, including
+        Original Medicare Part B, select Medicare Advantage plans, Medicare
+        Supplement or{" "}
+        <Link href="/" className="link">
+          Medigap plans and Medicare-Medicaid
+        </Link>{" "}
+        Plans.
+      </>
+    ),
+  },
+];
+
+export default accordionItems;
